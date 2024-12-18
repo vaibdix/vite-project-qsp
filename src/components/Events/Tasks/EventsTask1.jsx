4fae53ded664ece993a1710b3bf980dd98d876f3
vaibdix <dixitvaib@gmail.com>
2024-12-18 12:32:56 +0530

    chore: event task

---
+++ src/components/Events/Tasks/EventsTask1.jsx
@@ -0,0 +1,47 @@
+// EVENTS ASSIGNMENT TASK 1 -- using events and target
+
+const EventsTask1 = () => {
+  const handleClick = (e) => {
+    console.log(e);
+    e.target.disabled = true;
+    e.target.style.background = 'red';
+    e.target.textContent = 'btn disabled';
+  };
+
+  return (
+    <div>
+      <button
+        className="text-white bg-gray-800  focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 "
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default EventsTask1;
+
+
+
+// EVENTS ASSIGNMENT TASK 1 -- using USESTATE HOOK
+
+// import React, { useState } from 'react';
+// const EventsTask1 = () => {
+//   const [isDisabled, setIsDisabled] = useState(false);
+//   const handleClick = () => {
+//     setIsDisabled(true);
+//   };
+//   return (
+//     <div>
+//       <button
+//         className="text-white bg-gray-800  focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 "
+//         onClick={handleClick}
+//         disabled={isDisabled}
+//       >
+//         {isDisabled ? 'Button Disabled' : 'Think Before You Click'}
+//       </button>
+//     </div>
+//   );
+// };
+// export default EventsTask1;
---
+++ src/components/Events/Tasks/EventsTask2.jsx
@@ -0,0 +1,26 @@
+const EventsTask2 = () => {
+  const handleOnMouseEnter = (e) => {
+    console.log(e);
+    e.target.style.background = 'red';
+  };
+
+  const handleOnMouseLeave = (e) => {
+    console.log(e);
+    e.target.style.background = 'magenta';
+  };
+  return (
+    <div>
+      <button
+        id="eventsTask"
+        onMouseEnter={handleOnMouseEnter}
+        onMouseLeave={handleOnMouseLeave}
+      >
+        Hover Me
+      </button>
+    </div>
+  );
+};
+
+export default EventsTask2;
+
+
