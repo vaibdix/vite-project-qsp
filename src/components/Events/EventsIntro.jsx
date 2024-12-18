2065c239d133538d699cf8981cbb5cd844592a79
vaibdix <dixitvaib@gmail.com>
2024-12-18 08:44:40 +0530

    chore: events start

---
+++ src/components/Events/EventsIntro.jsx
@@ -0,0 +1,8 @@
+const EventsIntro = () => {
+  return (
+    <div>
+      <h1>Events Example 1</h1>
+    </div>
+  );
+}
+export default EventsIntro;
---
+++ src/components/Events/Examples/EventsEx1.jsx
@@ -0,0 +1,8 @@
+const EventsEx1 = () => {
+  return (
+    <div>
+      <h1>Events Example 1</h1>
+    </div>
+  );
+}
+export default EventsEx1;
