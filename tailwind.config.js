c1844522a89cc9605063a83d7a83f20a9fedb539
vaibdix <dixitvaib@gmail.com>
2024-12-18 21:34:13 +0530

    chore: new tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-     "./index.html",
-     "./src/**/*.{js,ts,jsx,tsx}",
-   ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'scale(0.9)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+      },
+    },
   },
   plugins: [],
-}
+};
